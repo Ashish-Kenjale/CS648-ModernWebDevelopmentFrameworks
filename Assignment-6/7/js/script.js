56ab0a17e7afa38bc8e293d05c2bd0a7b46597a1
Ashish Kenjale <ashishkenjale@gmail.com>
2019-10-04 16:51:01 -0700

    Assignment 6 Part 7 (Marco! Polo!)

---
+++ Assignment-6/7/js/script.js
@@ -0,0 +1,10 @@
+// Marco! Polo! (5 points)
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 == 0 && index % 5 == 0) {
+        window.console.log("Marco! Polo!");
+    } else if (index % 3 == 0) {
+        window.console.log("Marco!");
+    } else if (index % 5 == 0) {
+        window.console.log("Polo!");
+    }
+}
