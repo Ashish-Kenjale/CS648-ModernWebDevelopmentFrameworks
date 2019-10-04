afbda96da890d28a9833007585d56450111c03da
Ashish Kenjale <ashishkenjale@gmail.com>
2019-10-04 16:50:39 -0700

    Assignment 6 Part 6 (Even or Odd)

---
+++ Assignment-6/6/js/script.js
@@ -0,0 +1,9 @@
+// Odd or Even? (5 points)
+
+for (let index = 0; index < 16; index++) {
+    if (index % 2 == 0) {
+        window.console.log(index + " is even");
+    } else {
+        window.console.log(index + " is odd");
+    }
+}
