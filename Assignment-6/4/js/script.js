ea85ab41326f6117012ed24db51d3f6e7f7ab815
Ashish Kenjale <ashishkenjale@gmail.com>
2019-10-04 16:49:36 -0700

    Assignment 6 Part 4 (The “Coin Flip Streak” Game)

---
+++ Assignment-6/4/js/script.js
@@ -0,0 +1,13 @@
+// The “Coin Flip Streak” Game (5 points)
+var coinFlip;
+
+// Loop until coinFlip becomes 1 (Tails)
+do {
+    coinFlip = Math.round(Math.random());
+
+    if (coinFlip == 0) {
+        window.console.log("Heads");
+    } else if (coinFlip == 1) {
+        window.console.log("Tails");
+    }
+} while (coinFlip != 1);
